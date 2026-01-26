21ff7c5fc43cff9876ec1ff8b5e27d3e568b0f64
jonkmatsumo <jonkmatsumo@gmail.com>
2026-01-26 10:23:06 -0500

    Explicitly allow 127.0.0.1 origins in CORS configuration

--- src/agent_service/app.py
+++ src/agent_service/app.py
@@ -14,7 +14,14 @@ app = FastAPI(title="Text2SQL Agent Service")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:8501"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:8501",
+        "http://127.0.0.1:8501",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- src/mcp_server/main.py
+++ src/mcp_server/main.py
@@ -221,8 +221,11 @@ try:
                     CORSMiddleware,
                     allow_origins=[
                         "http://localhost:5173",
+                        "http://127.0.0.1:5173",
                         "http://localhost:3000",
+                        "http://127.0.0.1:3000",
                         "http://localhost:8501",
+                        "http://127.0.0.1:8501",
                     ],
                     allow_credentials=True,
                     allow_methods=["*"],
