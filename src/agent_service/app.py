28f2f48155449789801cb5e4068b83b7d7827fd4
jonkmatsumo <jonkmatsumo@gmail.com>
2026-01-26 10:48:09 -0500

    Configure Agent Service DB environment and refine CORS allowed origins

--- src/agent_service/app.py
+++ src/agent_service/app.py
@@ -21,8 +21,6 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8501",
         "http://127.0.0.1:8501",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
